165768be3eb3086e3f6b77c0e32b97ed2f2cc57a
dialejo <diegomaldonadom1992@gmail.com>
2025-01-21 18:46:22 -0500

    numero randon función

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
+let numeroSecreto = generarNumeroSecreto();
 
 function asignarTextoElemento(elemento, texto){
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
+    return;
 }
 
 function intentoDeUsuario() {
     alert('click desde el botón');
 }
+
+function generarNumeroSecreto() {
+    return Math.floor(Math.random() * 10) + 1;
+}  
 
 asignarTextoElemento('h1', 'Juego del numero secreto?');
 asignarTextoElemento('p', 'Adivina el número entre 1 y 100');
