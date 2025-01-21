2df5d2d1c5bc0096475edd119dd4cbbc2d2c3391
dialejo <diegomaldonadom1992@gmail.com>
2025-01-20 22:25:16 -0500

    eventos

---
+++ app.js
@@ -0,0 +1,5 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'juego del numero secreto';
+
+let parrafo =document.querySelector('p');
+parrafo.innerHTML = 'Adivina el numero entre 1 y 100';
